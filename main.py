09d7749f9760ce1f6d3a24930f94e2637f94f76a
obieuz <robizaj2006@gmail.com>
2024-12-21 16:39:53 +0100

    video and audio works

---
+++ main.py
@@ -0,0 +1,122 @@
+import requests
+import requests.auth
+from moviepy.video.tools.subtitles import SubtitlesClip
+from translate import Translator
+import pyttsx3
+from moviepy import *
+
+
+import resources.settings as settings
+
+
+def get_token():
+    auth = requests.auth.HTTPBasicAuth(settings.clientId, settings.secret)
+    data = {
+        "grant_type": "password",
+        "username": settings.username,
+        "password": settings.password
+    }
+    headers = {
+        "User-Agent": "OAuth2 Test Client"
+    }
+    res = requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers)
+    res = res.json()
+    return res.get("access_token")
+
+def get_story(subreddit="stories", limit=1):
+    headers = {
+        "Authorization": f"bearer {get_token()}",
+        "User-Agent": "OAuth2 Test Client"
+    }
+
+    params = {
+        "limit": limit
+    }
+
+    req = requests.get(f"https://oauth.reddit.com/r/{subreddit}/hot", params=params, headers=headers)
+    req = req.json()
+    return req.get("data").get("children")[:limit]
+
+def separate_characters(text):
+    parts = []
+    max_length = 500
+
+    while len(text) > max_length:
+        parts.append(text[:max_length])
+        text = text[max_length:]
+    parts.append(text)
+    return parts
+
+def separate_story(text):
+    parts = []
+
+    separated_text = separate_characters(text)
+    print(separated_text)
+
+
+    for text_string in separated_text:
+        count_of_words = len(text_string.split())
+
+        if not count_of_words < 180:
+            part = count_of_words // 180
+            for i in range(part):
+                parts.append(" ".join(text_string.split()[i * 180:(i + 1) * 180]))
+
+        parts.append(text_string)
+    print(parts)
+    return parts
+
+def translate_story(story, language):
+    separated_text = separate_characters(story)
+
+    translated_text = ""
+
+    translator = Translator(to_lang=language)
+
+    for text in separated_text:
+        translated_text += translator.translate(text)
+
+    return translated_text
+
+def create_speech(text,language="en"):
+    engine = pyttsx3.init()
+
+    engine.setProperty("rate", settings.words_per_minute)
+
+    voices = engine.getProperty('voices')
+    for voice in voices:
+        if language == "pl":
+            if voice.name == "Microsoft Paulina Desktop - Polish":
+                engine.setProperty("voice", voice.id)
+                break
+        if voice.name == "Microsoft David Desktop - English (United States)":
+            engine.setProperty("voice", voice.id)
+            break
+
+    engine.save_to_file(text, "story.mp3")
+    engine.runAndWait()
+    engine.stop()
+
+def create_video():
+    background = VideoFileClip("resources\\background\\background.mp4")
+    audio = AudioFileClip("story.mp3")
+    audio.duration = background.duration
+
+    video = CompositeVideoClip([background])
+
+    video.audio = audio
+
+    video.write_videofile("tiktok.mp4", codec="libx264", audio_codec="aac")
+
+
+
+
+#text = translate_story(get_story()[0].get("data").get("selftext"), "pl")
+
+text = get_story()[0].get("data").get("selftext")
+
+create_speech(text)
+
+create_video()
+
+# create_speech(text,"pl")
---
+++ resources/settings.py
@@ -0,0 +1,6 @@
+clientId = "7k3nBUE9VBMq1OhGM_CDCw"
+secret = "WhdvAUK4R8SpM19mwZx3yPPSonsGIQ"
+username = "obi_trash"
+password = "obieuztrash"
+words_per_minute = 180
+
