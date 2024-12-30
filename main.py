bfbfc6a0830800abe1e6f6867cb11eb37f867bee
obieuz <robizaj2006@gmail.com>
2024-12-30 01:34:10 +0100

    Fixing #7

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import requests
 import requests.auth
-from selenium import webdriver
 from moviepy.video.tools.subtitles import SubtitlesClip
-from selenium.webdriver.common.by import By
 from translate import Translator
 import pyttsx3
 from moviepy import *
 import json
+import whisper
 
-import resources.settings as settings
+import settings as settings
 
 
 def get_reddit_token():
-    auth = requests.auth.HTTPBasicAuth(settings.clientId, settings.secret)
+    auth = requests.auth.HTTPBasicAuth(settings.REDDIT_CLIENT_ID, settings.REDDIT_SECRET)
     data = {
         "grant_type": "password",
-        "username": settings.username,
-        "password": settings.password
+        "username": settings.REDDIT_USERNAME,
+        "password": settings.REDDIT_PASSWORD
     }
     headers = {
         "User-Agent": "OAuth2 Test Client"
@@ -25,25 +24,28 @@ def get_reddit_token():
     res = res.json()
     return res.get("access_token")
 
+
 def get_tiktok_code():
     return input("Enter the code: ")
 
+
 def get_tiktok_token():
     body = {
-        "client_key": settings.tiktok_client_key,
-        "client_secret": settings.tiktok_secret,
+        "client_key": settings.TIKTOK_CLIENT_ID,
+        "client_secret": settings.TIKTOK_SECRET,
         "code": get_tiktok_code(),
         "grant_type": "authorization_code",
-        "redirect_uri": settings.tiktok_redirect_url
+        "redirect_uri": settings.TIKTOK_REDIRECT_URL
     }
     headers = {
-        "Content-Type":"application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded"
     }
 
     res = requests.post("https://open.tiktokapis.com/v2/oauth/token/", data=body, headers=headers)
     res = res.json()
     print(res)
     return res.get("access_token")
+
 
 def get_story(subreddit="stories", limit=1):
     headers = {
@@ -55,27 +57,42 @@ def get_story(subreddit="stories", limit=1):
         "limit": limit
     }
 
-    req = requests.get(f"https://oauth.reddit.com/r/{subreddit}/hot", params=params, headers=headers)
-    req = req.json()
+    try:
 
-    story = req.get("data").get("children")[len(req.get("data").get("children")) - 1]
+        req = requests.get(f"https://oauth.reddit.com/r/{subreddit}/hot", params=params, headers=headers)
 
-    if not check_story(story):
-        return get_story(subreddit, limit+1)
+        req.raise_for_status()
 
-    return story
+        req = req.json()
+
+        story = req.get("data").get("children")[len(req.get("data").get("children")) - 1]
+
+        if not check_story(story):
+            return get_story(subreddit, limit + 1)
+
+        text = story.get("data").get("title") + ". " + story.get("data").get("selftext")
+        if len(text.split()) < settings.WORDS_PER_MINUTE:
+            return get_story(subreddit, limit + 1)
+
+        return story
+
+    except requests.RequestException as e:
+        print(e)
+        Exception("There was an error while getting the story : ", e)
+
 
 def check_story(story):
     data = story.get("data")
     subreddit_id = data.get("subreddit_id")
     post_id = data.get("id")
 
-    with open(settings.json_path, "r") as file:
+    with open(settings.STORAGE_PATH, "r") as file:
         stories = json.load(file)
         for story in stories:
             if story.get("subreddit_id") == subreddit_id and post_id in story.get("finished_stories"):
                 return False
     return True
+
 
 def add_finished_story(story):
     data = story.get("data")
@@ -83,7 +100,7 @@ def add_finished_story(story):
     post_id = data.get("id")
     subreddit_exist = False
 
-    with open(settings.json_path, "r") as file:
+    with open(settings.STORAGE_PATH, "r") as file:
         stories = json.load(file)
         for story in stories:
             if story.get("subreddit_id") == subreddit_id:
@@ -94,18 +111,19 @@ def add_finished_story(story):
         if not subreddit_exist:
             stories.append({"subreddit_id": subreddit_id, "finished_stories": [post_id]})
 
-    with open(settings.json_path, "w") as file:
+    with open(settings.STORAGE_PATH, "w") as file:
         json.dump(stories, file)
 
-def translate_story(text, language):
 
+def translate_story(text, language):
     translator = Translator(to_lang=language)
     return translator.translate(text)
+
 
 def create_speech(text, language="en"):
     engine = pyttsx3.init()
 
-    engine.setProperty("rate", settings.words_per_minute)
+    engine.setProperty("rate", settings.WORDS_PER_MINUTE)
 
     voices = engine.getProperty('voices')
     for voice in voices:
@@ -116,49 +134,79 @@ def create_speech(text, language="en"):
         if voice.name == "Microsoft David Desktop - English (United States)":
             engine.setProperty("voice", voice.id)
             break
+        if voice.name == "Microsoft Zira Desktop - English (United States)":
+            engine.setProperty("voice", voice.id)
+            break
 
-    engine.save_to_file(text, settings.speech_path)
+    engine.save_to_file(text, settings.AUDIO_PATH)
     engine.runAndWait()
     engine.stop()
 
+
 def create_video(subreddit="stories"):
     story = get_story(subreddit)
+    print("Got story")
     text = story.get("data").get("title") + ".\n" + story.get("data").get("selftext")
 
-    text = text.replace("\n", " ")
-    text = text.replace("."," ")
-    text = text.replace(","," ")
+    text = text.replace(".", " ")
+    text = text.replace(",", " ")
 
     create_speech(text)
+    print("Created audio")
 
-    subtitles = create_subtitles(text)
+    subtitles = generate_subtitles()
+    print("Created subtitles")
 
-    background = VideoFileClip(settings.background_path)
+    background = VideoFileClip(settings.BACKGROUND_VIDEO_PATH)
 
-    audio = AudioFileClip(settings.speech_path)
+    audio = AudioFileClip(settings.AUDIO_PATH)
+    if audio.duration > background.duration:
+        audio.duration = background.duration
 
     video = CompositeVideoClip([background, subtitles.with_position(("center", "center"))])
     video.audio = audio
     video.duration = background.duration
 
     add_finished_story(story)
+    print("Added story to finished stories")
 
-    video.write_videofile(settings.video_path, codec="libx264", fps=settings.fps)
+    video.write_videofile(settings.RESULT_VIDEO_PATH, codec="libx264", fps=settings.FPS)
+    print("Created video")
 
-def create_subtitles(text_list):
-    text = "".join(text_list).split()
 
-    subtitle_list = []
-    count_of_words = len(text)
+def generate_subtitles():
+    model = whisper.load_model("base")
+    result = model.transcribe(settings.AUDIO_PATH, fp16=False)
 
-    for i in range(count_of_words // settings.words_per_second):
-        text_clip_text = " ".join(text[i * settings.words_per_second:(i + 1) * settings.words_per_second])
-        subtitle_list.append(((i,i+1),text_clip_text))
+    srt_content = []
+    for i, segment in enumerate(result["segments"]):
+        start = segment["start"]
+        end = segment["end"]
+        text = segment["text"]
 
-    generator = lambda txt: TextClip(text=txt, font=settings.font_path, font_size=settings.font_size,
-                                     color=settings.font_color)
+        start_time = format_time(start)
+        end_time = format_time(end)
 
-    return SubtitlesClip(subtitle_list, make_textclip=generator)
+        srt_content.append(f"{i + 1}")
+        srt_content.append(f"{start_time} --> {end_time}")
+        srt_content.append(text)
+        srt_content.append("")
+
+    with open(settings.SUBTITLES_PATH, "w", encoding="utf-8") as f:
+        f.write("\n".join(srt_content))
+
+    generator = lambda txt: TextClip(text=txt, font=settings.FONT_PATH,
+                                     color=settings.FONT_COLOR, size=(1920, 1080))
+    return SubtitlesClip(settings.SUBTITLES_PATH, make_textclip=generator)
+
+
+def format_time(seconds):
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    seconds = int(seconds % 60)
+    milliseconds = int((seconds % 1) * 1000)
+    return f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
+
 
 def get_user_info():
     url = "https://open-api.tiktok.com/v2/user/info/"
@@ -172,5 +220,5 @@ def get_user_info():
     res = requests.get(url, params=params, headers=headers)
     print(res.json())
 
-get_user_info()
 
+create_video()
